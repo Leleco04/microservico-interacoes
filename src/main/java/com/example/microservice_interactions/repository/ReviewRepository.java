45a6f2b38d9cf4ed0d3333290ace8aa994e39930
Leandro Hideki <htwleandro@gmail.com>
2025-07-17 14:08:25 -0300

    Add files via upload

--- src/main/java/com/example/microservice_interactions/repository/ReviewRepository.java
+++ src/main/java/com/example/microservice_interactions/repository/ReviewRepository.java
@@ -3,6 +3,8 @@ package com.example.microservice_interactions.repository;
 import com.example.microservice_interactions.entity.Like;
 import com.example.microservice_interactions.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByUserId(Long userId);
 
     List<Review> findByBookId(Long bookId);
+
+    long countByBookId(Long bookId);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.bookId = :bookId")
+    Double findAverageRatingByBookId(@Param("bookId") Long bookId);
 }
